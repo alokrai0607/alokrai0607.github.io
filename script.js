5ca34cf7967f18ab59b9921b5172e80621192892
alokrai0607 <saelalok001@gmail.com>
2023-06-05 18:58:51 +0530

    Resume Update

--- script.js
+++ script.js
@@ -2,6 +2,29 @@ function myfunction(){
   document.getElementById("mytopnav").classList.toggle("responsive");
   
 }
+function sub(){
+  event.preventDefault()
+  alert("hello")
+  emailjs.send("service_qanxv6w","template_5g7ruee",{
+    from_name: "saurabh",
+    message: "Hello dear",
+    },"h8s4g7h4yAgMruUze");
+
+  // var templateParams = {
+  //   from_name: 'abc',
+  //   message: 'Please Find out the attached file'
+  // };
+
+    // emailjs.send("service_qanxv6w", "template_5g7ruee", templateParams)
+    // .then(function(response) {
+    //   console.log('SUCCESS!', response.status, response.text);
+    // }, function(error) {
+    //   console.log('FAILED...', error);
+    // });
+
+   
+}
+
 //navbar sticky
 window.onscroll = function() {myFunction()};
 
@@ -24,3 +47,5 @@ GitHubCalendar(".calendar", "alokrai0607", { responsive: true });
 function resume(){
 window.open("https://drive.google.com/file/d/1Qdwd40KmkByDwamw2tOvhtXyXZC9T2hE/view?usp=sharing", "_blank");
 }
+
+

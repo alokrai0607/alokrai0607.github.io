c2d6e6ec252b99c4906e033762bfbe9b221ead56
alokrai0607 <saelalok001@gmail.com>
2023-06-07 13:59:47 +0530

    Hello World

--- script.js
+++ script.js
@@ -6,23 +6,9 @@ function sub(){
   event.preventDefault()
   alert("hello")
   emailjs.send("service_qanxv6w","template_5g7ruee",{
-    from_name: "saurabh",
-    message: "Hello dear",
-    },"h8s4g7h4yAgMruUze");
-
-  // var templateParams = {
-  //   from_name: 'abc',
-  //   message: 'Please Find out the attached file'
-  // };
-
-    // emailjs.send("service_qanxv6w", "template_5g7ruee", templateParams)
-    // .then(function(response) {
-    //   console.log('SUCCESS!', response.status, response.text);
-    // }, function(error) {
-    //   console.log('FAILED...', error);
-    // });
-
-   
+    from_name: "Alok",
+    message: "Hello !",
+    },"h8s4g7h4yAgMruUze");   
 }
 
 //navbar sticky

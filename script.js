661ac8757588fbeab43ccdfc9f7fee5f30e3ed01
alokrai0607 <saelalok001@gmail.com>
2023-06-09 15:49:00 +0530

    Portfolio

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function myfunction(){
   document.getElementById("mytopnav").classList.toggle("responsive");
-  
 }
 function sub(){
   event.preventDefault()
@@ -24,11 +23,9 @@ if (window.pageYOffset >= sticky) {
   mytopnav.classList.remove("sticky");
 }
 }
-
 GitHubCalendar(".calendar", "alokrai0607");
 // or enable responsive functionality
 GitHubCalendar(".calendar", "alokrai0607", { responsive: true });
-
 function resume(){
 window.open("https://drive.google.com/file/d/1opUM57d_nAP79bhtp4pt9JmgjPraEWan/view?usp=sharing", "_blank");
 }

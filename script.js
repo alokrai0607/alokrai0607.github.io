d0ac1a1544e6fc3f828d82fd0b80a8f3a58249cc
alokrai0607 <saelalok001@gmail.com>
2023-06-28 00:06:44 +0530

    Resume Updated

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ function sub(){
   alert("hello")
   emailjs.send("service_qanxv6w","template_5g7ruee",{
     from_name: "Alok",
-    message: "Hello !",
+    message: "Hello",
     },"h8s4g7h4yAgMruUze");   
 }
-GitHubCalendar(".calendar", "alokrai0607");
+GitHubCalendar(".calendar","alokrai0607");
 // or enable responsive functionality
 GitHubCalendar(".calendar", "alokrai0607", { responsive: true });
 function resume(){
-window.open("https://drive.google.com/file/d/1opUM57d_nAP79bhtp4pt9JmgjPraEWan/view?usp=sharing", "_blank");
+window.open("https://drive.google.com/file/d/1WQAyIQIlABgq4lI2LyWHSRUg6amCNabf/view?usp=sharing", "_blank");
 }
 
 

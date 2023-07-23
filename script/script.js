b809bacaea19283f3c939cde5bbbc845bbdc4500
alokrai0607 <saelalok001@gmail.com>
2023-07-23 20:14:26 +0530

    Some Changes

--- script/script.js
+++ script/script.js
@@ -2,6 +2,7 @@
 const navMenu = document.getElementById("nav-menu");
 navToggle = document.getElementById("nav-toggle");
 navClose = document.getElementById("nav-close");
+
 if (navToggle) {
     navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu');
@@ -13,17 +14,23 @@ if (navClose) {
     })
 }
 
+
+
 /*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll(".nav_link");
+
 function linkAction() {
     const navMenu = document.getElementById("nav-menu");
     navMenu.classList.remove('show-menu');
 }
-
 navLink.forEach(n => n.addEventListener('click', linkAction));
+
+
+
 /*==================== SKILLS ====================*/
 const skillsContent = document.getElementsByClassName("skills_content");
 skillsHeader = document.querySelectorAll(".skills_header");
+
 function toggleSkills() {
     let itemClass = this.parentNode.className;
 
@@ -34,16 +41,17 @@ function toggleSkills() {
         this.parentNode.className = "skills_content skills_open";
     }
 }
+
 skillsHeader.forEach((element) => {
     element.addEventListener('click', toggleSkills);
 })
 
 
 
-
 /*==================== QUALIFICATION ====================*/
 const tabs = document.querySelectorAll('[data-target]')
 tabContents = document.querySelectorAll('[data-content]')
+
 tabs.forEach(tab => {
     tab.addEventListener('click', () => {
         const target = document.querySelector(tab.dataset.target)
@@ -59,8 +67,6 @@ tabs.forEach(tab => {
         tab.classList.add('qualification_active')
     })
 })
-
-
 
 
 
@@ -82,12 +88,10 @@ function scrollActive() {
         }
     })
 }
-
-
-
-
-
 window.addEventListener('scroll', scrollActive)
+
+
+
 /*==================== CHANGE BACKGROUND HEADER ====================*/
 function scrollHeader() {
     const nav = document.getElementById('header')
@@ -95,8 +99,6 @@ function scrollHeader() {
     if (this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
-
-
 
 
 
@@ -110,24 +112,26 @@ window.addEventListener('scroll', scrollUp)
 
 
 
-
-
 /*==================== DARK LIGHT THEME ====================*/
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'uil-sun'
+
 // Previously selected topic (if user selected)
 const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
+
 // We obtain the current theme that the interface has by validating the dark-theme class
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
 const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
 // We validate if the user previously chose a topic
 if (selectedTheme) {
     // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
     document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
     themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
 }
+
 // Activate / deactivate the theme manually with the button
 themeButton.addEventListener('click', () => {
     // Add or remove the dark / icon theme

1d5d8178150a970d1db4f7ea66cb337b13e4444e
alokrai0607 <saelalok001@gmail.com>
2023-08-02 14:49:34 +0530

    Some changes

--- script/script.js
+++ script/script.js
@@ -112,6 +112,7 @@ const iconTheme = 'uil-sun'
 
 // Previously selected topic (if user selected)
 const selectedTheme = localStorage.getItem('selected-theme')
+
 const selectedIcon = localStorage.getItem('selected-icon')
 
 // We obtain the current theme that the interface has by validating the dark-theme class
@@ -125,6 +126,7 @@ const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-mo
 if (selectedTheme) {
     // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
     document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+
     themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
 }
 
@@ -133,10 +135,12 @@ themeButton.addEventListener('click', () => {
     
     // Add or remove the dark / icon theme
     document.body.classList.toggle(darkTheme)
+
     themeButton.classList.toggle(iconTheme)
 
     // We save the theme and the current icon that the user chose
     localStorage.setItem('selected-theme', getCurrentTheme())
+
     localStorage.setItem('selected-icon', getCurrentIcon())
 
 

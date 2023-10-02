f9e6f33031fd44fb368917fd21ed0c900ae70559
alokrai0607 <saelalok001@gmail.com>
2023-10-02 21:00:55 +0530

    Change

--- script/script.js
+++ script/script.js
@@ -137,6 +137,8 @@ themeButton.addEventListener('click', () => {
 
     themeButton.classList.toggle(iconTheme)
 
+
+    
     // We save the theme and the current icon that the user chose
     localStorage.setItem('selected-theme', getCurrentTheme())
 

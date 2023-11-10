df92faadc1d2a5083da2dcb625e6744a19f40f39
alokrai0607 <saelalok001@gmail.com>
2023-11-10 22:21:42 +0530

    P

--- script/script.js
+++ script/script.js
@@ -123,6 +123,7 @@ const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
 
 // We obtain the current theme that the interface has by validating the dark-theme class
+
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
 
 const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
@@ -132,6 +133,7 @@ const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-mo
 // We validate if the user previously chose a topic
 if (selectedTheme) {
     // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    
     document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
     
     themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)

e9b306a05d6282234d3aabcd00244dce8effc6ec
alokrai0607 <saelalok001@gmail.com>
2023-11-11 22:52:20 +0530

    do

--- script/script.js
+++ script/script.js
@@ -129,7 +129,6 @@ const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dar
 const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
 
 
-
 // We validate if the user previously chose a topic
 if (selectedTheme) {
     // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
@@ -143,7 +142,6 @@ themeButton.addEventListener('click', () => {
       
     // Add or remove the dark / icon theme
     document.body.classList.toggle(darkTheme)
-
 
     themeButton.classList.toggle(iconTheme)
 

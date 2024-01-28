19eb3afb77c162b23e07ca7474f8a003330ac141
alokrai0607 <saelalok001@gmail.com>
2024-01-28 13:47:40 +0530

    Hello portfolio

--- script/script.js
+++ script/script.js
@@ -72,11 +72,8 @@ function scrollActive() {
 
   sections.forEach((current) => {
     const sectionHeight = current.offsetHeight;
-
     const sectionTop = current.offsetTop - 50;
-
     sectionId = current.getAttribute("id");
-
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
       document
         .querySelector(".nav_menu a[href*=" + sectionId + "]")
@@ -94,7 +91,6 @@ window.addEventListener("scroll", scrollActive);
 function scrollHeader() {
   const nav = document.getElementById("header");
   // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
-
   if (this.scrollY >= 80) nav.classList.add("scroll-header");
   else nav.classList.remove("scroll-header");
 }

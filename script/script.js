878bc24c03980ff5b726a3e0e81f000426cd9c94
alokrai0607 <saelalok001@gmail.com>
2024-02-06 16:38:13 +0530

    portfolio

--- script/script.js
+++ script/script.js
@@ -101,13 +101,16 @@ function scrollUp() {
   const scrollUp = document.getElementById("scroll-up");
   // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
   if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+  
   else scrollUp.classList.remove("show-scroll");
 }
 window.addEventListener("scroll", scrollUp);
 
 /*==================== DARK LIGHT THEME ====================*/
 const themeButton = document.getElementById("theme-button");
+
 const darkTheme = "dark-theme";
+
 const iconTheme = "uil-sun";
 
 // Previously selected topic (if user selected)

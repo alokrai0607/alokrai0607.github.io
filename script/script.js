bcf6fa7cf55f93a8267d7616d6b4cfe891968c24
alokrai0607 <saelalok001@gmail.com>
2024-09-26 14:33:46 +0530

    Changes

--- script/script.js
+++ script/script.js
@@ -2,19 +2,16 @@
 const navMenu = document.getElementById("nav-menu");
 navToggle = document.getElementById("nav-toggle");
 navClose = document.getElementById("nav-close");
-
 if (navToggle) {
   navToggle.addEventListener("click", () => {
     navMenu.classList.add("show-menu");
   });
 }
-
 if (navClose) {
   navClose.addEventListener("click", () => {
     navMenu.classList.remove("show-menu");
   });
 }
-
 /*==================== REMOVE MENU MOBILE ====================*/
 
 const navLink = document.querySelectorAll(".nav_link");
